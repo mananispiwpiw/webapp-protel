8d6ca64b73f62d127f32e4a0e39ee47e6a8fdb88
Muhammad Rafi Raihandika <rafijaeger@gmail.com>
2023-04-02 14:19:01 +0700

    Adding Apexchart syntax for dummy chart.

---
+++ views/main/main.js
@@ -0,0 +1,18 @@
+// CHARTS
+var lineChartOptions = {
+    chart: {
+      type: 'line',
+      height: 250
+    },
+    series: [{
+      name: 'Data',
+      data: [28, 29, 33, 36, 32, 32, 33]
+    }],
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+    }
+  }
+  
+  var lineChart = new ApexCharts(document.querySelector("#line-chart"), lineChartOptions);
+  lineChart.render();
+  
