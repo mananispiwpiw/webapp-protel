955f6d2b50e61c2c98ff0d56d488f67b23a23ae3
Muhammad Rafi Raihandika <rafijaeger@gmail.com>
2023-04-09 23:22:07 +0700

    add ConnectToDatabase() function

--- initializers/db.go
+++ initializers/db.go
@@ -1,9 +1,20 @@
 package initializers
 
 import (
+	"fmt"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-  )
+)
 
-dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+var DB *gorm.DB
+
+func ConnectToDatabase() {
+	var err error
+	dsn := "host=localhost user=postgres password=admin dbname=webapp_protel port=5432"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+	}
+}
